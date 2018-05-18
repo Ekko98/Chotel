a06353639fad05ad2df2d796d29d6e02abfc37ff
Ekko <ekko98@aliyun.com>
2018-05-18 15:35:23 +0800

    基本图形化初步设计----李云龙

--- client.h
+++ client.h
@@ -1,4 +1,22 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <QWidget>
+
+namespace Ui {
+class Client;
+}
+
+class Client : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Client(QWidget *parent = 0);
+    ~Client();
+
+private:
+    Ui::Client *ui;
+};
+
 #endif // CLIENT_H
